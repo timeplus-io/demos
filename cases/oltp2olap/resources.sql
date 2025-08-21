3a59a55c7140a3702c8eb07e34ef82778b9d257b
Gang Tao <gang@timeplus.io>
2025-08-21 15:21:17 -0700

    update

--- cases/oltp2olap/resources.sql
+++ cases/oltp2olap/resources.sql
@@ -36,10 +36,66 @@ CREATE MUTABLE STREAM retailer_etl.dim_products
   `buyPrice` float32,
   `MSRP` float32
 )
-ENGINE = MutableStream
 PRIMARY KEY productCode;
 
+-- external table
+CREATE EXTERNAL TABLE retailer_etl.gcs
+(
+  `orderNumber` uint32,
+  `customerNumber` int16,
+  `orderDate` int16,
+  `status` string,
+  `orderTotal` decimal(10, 2),
+  `itemCount` uint64,
+  `productCodes` array(string),
+  `productNames` array(string),
+  `productLines` array(string)
+)
+SETTINGS type = 's3', 
+    endpoint = 'https://storage.googleapis.com/timeplus-demo', 
+    access_key_id = 'key', 
+    secret_access_key = 'secret', 
+    data_format = 'JSONEachRow', 
+    write_to = 'retailer_cdc/orders.jsonl', 
+    s3_min_upload_file_size = 1024, 
+    s3_max_upload_idle_seconds = 60;
+
+-- dictionary for products table in retailer database
+CREATE DICTIONARY retailer_etl.dict_products
+(
+  `productCode` string,
+  `productName` string,
+  `productLine` string,
+  `productVendor` string,
+  `productDescription` string,
+  `quantityInStock` int16,
+  `buyPrice` float32,
+  `MSRP` float32
+)
+PRIMARY KEY productCode
+SOURCE(MYSQL(DB 'retailer' TABLE 'products' HOST '35.247.93.97' PORT 3306 USER 'admin' PASSWORD 'password' BG_RECONNECT true))
+LAYOUT(MUTABLE_CACHE(DB 'retailer_etl' STREAM 'dim_products' UPDATE_FROM_SOURCE false));
+
 -- MV
+CREATE MATERIALIZED VIEW retailer_etl.mv_orders
+(
+  `_tp_time` datetime64(3, 'UTC'),
+  `orderNumber` uint32,
+  `orderDate` int16,
+  `requiredDate` int16,
+  `shippedDate` int16,
+  `status` string,
+  `comments` string,
+  `customerNumber` int16,
+  `_tp_sn` int64
+) AS
+SELECT
+  _tp_time, to_uint32_or_zero(raw:after.orderNumber) AS orderNumber, to_int16_or_zero(raw:after.orderDate) AS orderDate, to_int16_or_zero(raw:after.requiredDate) AS requiredDate, to_int16_or_zero(raw:after.shippedDate) AS shippedDate, raw:after.status AS status, raw:after.comments AS comments, cast(raw:after.customerNumber, 'int16') AS customerNumber
+FROM
+  retailer_etl.topic_orders
+SETTINGS
+  seek_to = 'earliest'
+
 CREATE MATERIALIZED VIEW retailer_etl.mv_load_products INTO retailer_etl.dim_products
 (
   `_tp_time` datetime64(3, 'UTC'),
@@ -59,6 +115,28 @@ SELECT
   raw:after.productName AS productName, raw:after.productLine AS productLine, raw:after.productVendor AS productVendor, raw:after.productDescription AS productDescription, cast(raw:after.quantityInStock, 'int16') AS quantityInStock, cast(raw:after.buyPrice, 'float32') AS buyPrice, cast(raw:after.MSRP, 'float32') AS MSRP
 FROM
   retailer_etl.topic_products
+SETTINGS
+  seek_to = 'earliest';
+
+CREATE MATERIALIZED VIEW retailer_etl.mv_orderdetails
+(
+  `_tp_time` datetime64(3, 'UTC'),
+  `orderNumber` uint32,
+  `productCode` string,
+  `quantityOrdered` int16,
+  `priceEach` float32,
+  `orderLineNumber` int16,
+  `_tp_sn` int64
+) AS
+SELECT
+  _tp_time, 
+  to_uint32_or_zero(raw:after.orderNumber) AS orderNumber, 
+  raw:after.productCode AS productCode, 
+  cast(raw:after.quantityOrdered, 'int16') AS quantityOrdered, 
+  cast(raw:after.priceEach, 'float32') AS priceEach, 
+  cast(raw:after.orderLineNumber, 'int16') AS orderLineNumber
+FROM
+  retailer_etl.topic_orderdetails
 SETTINGS
   seek_to = 'earliest';
 
@@ -117,64 +195,3 @@ FROM
 GROUP BY
   window_start, orderNumber)
 COMMENT 'Combine details for same order & create JSON in GCS, batch every 30s';
-
-CREATE MATERIALIZED VIEW retailer_etl.mv_orderdetails
-(
-  `_tp_time` datetime64(3, 'UTC'),
-  `orderNumber` uint32,
-  `productCode` string,
-  `quantityOrdered` int16,
-  `priceEach` float32,
-  `orderLineNumber` int16,
-  `_tp_sn` int64
-) AS
-SELECT
-  _tp_time, 
-  to_uint32_or_zero(raw:after.orderNumber) AS orderNumber, 
-  raw:after.productCode AS productCode, 
-  cast(raw:after.quantityOrdered, 'int16') AS quantityOrdered, 
-  cast(raw:after.priceEach, 'float32') AS priceEach, 
-  cast(raw:after.orderLineNumber, 'int16') AS orderLineNumber
-FROM
-  retailer_etl.topic_orderdetails
-SETTINGS
-  seek_to = 'earliest';
-
--- external table
-CREATE EXTERNAL TABLE retailer_etl.gcs
-(
-  `orderNumber` uint32,
-  `customerNumber` int16,
-  `orderDate` int16,
-  `status` string,
-  `orderTotal` decimal(10, 2),
-  `itemCount` uint64,
-  `productCodes` array(string),
-  `productNames` array(string),
-  `productLines` array(string)
-)
-SETTINGS type = 's3', 
-    endpoint = 'https://storage.googleapis.com/timeplus-demo', 
-    access_key_id = 'GOOG1EXR4JXMTFDFYRSH6DKX2CBDEQLNBKOSDKM474P2RIEJIVVGOUQ5VRKEZ', 
-    secret_access_key = 'AFNU7Ecb/xG/gnZ1ha6qxclElS7L/9vfK+D/YOsv', 
-    data_format = 'JSONEachRow', 
-    write_to = 'retailer_cdc/orders.jsonl', 
-    s3_min_upload_file_size = 1024, 
-    s3_max_upload_idle_seconds = 60;
-
-
--- dictionary for products table in retailer database
-CREATE DICTIONARY retailer_etl.dict_products
-(
-  `productCode` string,
-  `productName` string,
-  `productLine` string,
-  `productVendor` string,
-  `productDescription` string,
-  `quantityInStock` int16,
-  `buyPrice` float32,
-  `MSRP` float32
-)
-PRIMARY KEY productCode
-SOURCE(MYSQL(DB 'retailer' TABLE 'products' HOST '35.247.93.97' PORT 3306 USER 'admin' PASSWORD 'BB/IRAbg12ZMcH' BG_RECONNECT true))
-LAYOUT(MUTABLE_CACHE(DB 'retailer_etl' STREAM 'dim_products' UPDATE_FROM_SOURCE false));
