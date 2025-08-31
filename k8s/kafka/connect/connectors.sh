05845506b065b12fcad53d2be2b1032f7a1abda2
Gang Tao <gang@timeplus.io>
2025-08-31 14:34:07 -0700

    add connector creation script

---
+++ k8s/kafka/connect/connectors.sh
@@ -0,0 +1,27 @@
+curl -X POST http://localhost:8083/connectors \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "mysql-retailer-cdc-connector",
+    "config": {
+      "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+      "database.hostname": "35.247.93.97",
+      "database.port": "3306",
+      "database.user": "admin",
+      "database.password": "Password!",
+      "database.server.id": "12345",
+      "topic.prefix": "demo.cdc.mysql.retailer",
+      "database.include.list": "retailer",
+      "table.include.list": "retailer.orders,retailer.orderdetails,retailer.products",
+      "schema.history.internal.kafka.bootstrap.servers": "10.138.0.23:9092:9092",
+      "schema.history.internal.kafka.topic": "demo.cdc.mysql.retailer.schema-changes",
+      "include.schema.changes": "true",
+      "transforms": "route",
+      "transforms.route.type": "org.apache.kafka.connect.transforms.RegexRouter",
+      "transforms.route.regex": "demo.cdc.mysql.retailer.retailer.(.*)",
+      "transforms.route.replacement": "demo.cdc.mysql.retailer.$1",
+      "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "key.converter.schemas.enable": "false",
+      "value.converter.schemas.enable": "false"
+    }
+  }'
