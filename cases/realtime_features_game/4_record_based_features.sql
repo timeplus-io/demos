09e81b322f1151967f0d218103cea2f123a82f01
Gang Tao <gang@timeplus.io>
2025-10-14 11:50:15 -0700

    update

---
+++ cases/realtime_features_game/4_record_based_features.sql
@@ -0,0 +1,45 @@
+
+-- total spend in last 10 transactions
+CREATE STREAM game.total_spend_last_10_transaction
+(
+  `user_id` string,
+  `total_spend` float64
+)
+TTL to_datetime(_tp_time) + INTERVAL 24 HOUR
+SETTINGS logstore_retention_bytes = '107374182', logstore_retention_ms = '300000';
+
+CREATE MATERIALIZED VIEW game.mv_total_spend_last_10_transaction
+INTO game.total_spend_last_10_transaction
+AS
+select 
+    user_id, 
+    array_sum(x->x, group_array_last (amount_usd, 10)) as total_spend
+from game.transactions
+group by user_id
+
+----------------------------------------------------------------------------------------
+
+-- total survival time in last 7 games
+CREATE MATERIALIZED VIEW game.mv_total_survival_last_7_game AS
+select 
+    user_id, 
+    array_sum(x->x, group_array_last (event_data:survival_time::int, 7)) as total_survival_time_in_last_7_games
+from game.player_actions
+where event_type = 'match_end'
+group by user_id;
+
+
+-- feature 5 lost in a row last 5 games
+CREATE MATERIALIZED VIEW game.mv_5_lost_in_a_row AS
+select 
+    user_id, 
+    group_array_last(event_data:result, 5) as last_five_game_result,
+    case 
+        when length(last_five_game_result) = 5 
+             and array_count(x -> x = 'lost', last_five_game_result) = 5 
+        then true 
+        else false 
+    end as all_five_lost
+from game.player_actions
+where event_type = 'match_end'
+group by user_id
---
+++ cases/realtime_features_game/7_alert.sql
@@ -0,0 +1,24 @@
+-- Alert UDF Definition
+CREATE OR REPLACE FUNCTION alert_to_slack(value string) RETURNS string LANGUAGE PYTHON AS $$
+import json
+import requests
+def alert_to_slack(value):
+    for val in value:
+        requests.post("https://hooks.slack.com/services/***", data=json.dumps({"text": f"{val}"}))
+    return value
+$$
+
+-- Alert if a player has lost 5 games in a row and spent more than $100 in last 10 transactions
+
+CREATE ALERT spending_alert
+BATCH 10 EVENTS WITH TIMEOUT 5s
+LIMIT 1 ALERTS PER 15s
+CALL alert_to_slack
+AS 
+SELECT concat(user_id, ' spend ', to_string(total_spend), ' in last 10 games') as value 
+FROM game.total_spend_last_10_transaction
+WHERE total_spend > 800
+
+
+-- 
+DROP ALERT game.spending_alert
